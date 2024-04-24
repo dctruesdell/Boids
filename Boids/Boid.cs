9ae7710ae781a20c9801ccabf4a5d528c4b05902
dctruesdell <144290233+dctruesdell@users.noreply.github.com>
2024-04-24 17:05:02 -0500

    FollowCenter
    added function for boids to follow the center of those they can see

--- Boids/Boid.cs
+++ Boids/Boid.cs
@@ -7,7 +7,7 @@ using Microsoft.Xna.Framework;
 
 namespace Boids
 {
-    internal class Boid
+    public class Boid
     {
         public Vector2 position;
         public Vector2 velocity;
@@ -29,13 +29,28 @@ namespace Boids
             get { return _sightRange; }
         }
 
-        public void avoid(Boid other)
+        public void Avoid(Boid other)
         {
             if (GroupFuncs.FindDistance(this.position, other.position)  <= _protectedRange)
             {
-                for 
+                Vector2 newVelocity = Vector2.Zero;
+                newVelocity += this.position - other.position;
+                this.velocity += newVelocity * SimulateNext.avoidFactor;
             }
         }
 
+        public void FollowCenter()
+        {
+            List<Vector2> nearbyBoids = new List<Vector2>();
+            
+            for (int i = 0; i < SimulateNext.boids.Count; i++)
+            {
+                if (GroupFuncs.FindDistance(this.position, SimulateNext.boids[i].position) <= _sightRange)
+                {
+                    nearbyBoids.Add(SimulateNext.boids[i].position);
+                }
+            }
+            this.velocity += GroupFuncs.AveragePosition(nearbyBoids) * SimulateNext.groupingFactor;
+        } 
     }
 }
--- Boids/SimulateNext.cs
+++ Boids/SimulateNext.cs
@@ -9,6 +9,7 @@ namespace Boids
     public static class SimulateNext
     {
         public static float avoidFactor = 3;
+        public static float groupingFactor = 3;
         public static List<Boid> boids = new List<Boid>();
 
 
